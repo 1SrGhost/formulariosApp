2c490cb74b5ef49cc353c20bafd6c7c93cb8c4e3
Andres Perez <edgar4698@hotmail.com>
2022-10-03 15:51:54 -0500

    validacion de campos por ts

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,20 +1,31 @@
-
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
-  styleUrls: ['./basicos.component.css']
+  styleUrls: ['./basicos.component.css'],
 })
 export class BasicosComponent implements OnInit {
+  @ViewChild('miFormulario') miFormulario!: NgForm;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+/**
+ * Si el control del producto no es válido y se ha tocado, devuelva verdadero
+ * @returns devolver (
+ *     this.miFormulario.controls['producto']?.invalid &&
+ *     este.miFormulario.controla['producto']?.tocado
+ *   );
+ */
+  nombreValido(): boolean {
+    return (
+      this.miFormulario?.controls['producto']?.invalid &&
+      this.miFormulario?.controls['producto']?.touched
+    );
   }
-  guardar(miFormulario:NgForm){
-    console.log('Guardar',miFormulario.value);
+  guardar() {
+    console.log('Guardar', this.miFormulario.value);
   }
-
 }
