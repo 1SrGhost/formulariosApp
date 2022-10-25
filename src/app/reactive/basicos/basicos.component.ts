b53a3368fe7496a7d3c16589048c51e6c8ca53ef
Andres Perez <edgar4698@hotmail.com>
2022-10-25 13:09:44 -0500

    uso del formBuilder

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,17 +1,23 @@
-import { Component} from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
-  styleUrls: ['./basicos.component.css']
+  styleUrls: ['./basicos.component.css'],
 })
-export class BasicosComponent  {
+export class BasicosComponent {
+/* Creando un nuevo FormGroup con 3 FormControls. */
+  // miFormulario: FormGroup = new FormGroup({
+  //   nombre: new FormControl('RTX 4080ti'),
+  //   precio: new FormControl(1500),
+  //   existencias: new FormControl(5),
+  // });
 
-  miFormulario:FormGroup = new FormGroup({
-    'nombre': new FormControl('RTX 4080ti')
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['RTX 4080ti'],
+    precio: [1500],
+    existencias: [5],
   })
-  constructor() { }
-
- 
+  constructor(private fb: FormBuilder) {}
 }
