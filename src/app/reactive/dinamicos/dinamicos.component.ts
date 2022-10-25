e04d2e3e11982280a22e524b4f138165629bfa35
Andres Perez <edgar4698@hotmail.com>
2022-10-25 15:46:08 -0500

    FormArray, mostrar elementos

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicosComponent implements OnInit {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [, [Validators.required, Validators.minLength(3)]],
+    favoritos: this.fb.array([
+      ['Metal Gear'],
+      ['Death Stranding']
+    ],Validators.required)
+   
+  });
+
+  get favoritosArr(){
+    return this.miFormulario.get('favoritos') as FormArray;
+  }
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
   }
+  validarCampo(campo: string) {
+    return (
+      this.miFormulario.controls[campo].errors &&
+      this.miFormulario.controls[campo].touched
+    );
+  }
+  guardar(){
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+    this.miFormulario.reset();
+  }
+  
 
 }
